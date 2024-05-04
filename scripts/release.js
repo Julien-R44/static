a29a9a880156218aa28efc6e4b4962c978f509e8
Julien Ripouteau <julien@ripouteau.com>
2024-05-04 17:22:23 +0200

    chore: update script

--- scripts/release.js
+++ scripts/release.js
@@ -1,10 +1,14 @@
+const shouldUpdateRepo = !process.argv.includes("--no-update-repo");
+
 console.log("Building sponsors.svg...");
 await $`pnpm sponsorkit`;
 
 console.log("Uploading to R2...");
-await $`pnpm wrangler r2 object put static/sponsors.svg --file=sponsorkit/sponsors.svg`;
+await $`pnpm wrangler r2 object put static/sponsors.png --file=sponsorkit/sponsors.png --content-type=image/png`;
 
-console.log("Updating repo...");
-await $`git add . && git commit -m "Update sponsors.png" && git push`;
+if (shouldUpdateRepo) {
+  console.log("Updating repo...");
+  await $`git add . && git commit -m "Update sponsors.png" && git push`;
+}
 
 console.log("Done!");
