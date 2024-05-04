0aed2397da53e75763413f0ceccf481537e9df6d
Julien Ripouteau <julien@ripouteau.com>
2024-05-04 17:18:10 +0200

    Update sponsors.png

--- scripts/release.js
+++ scripts/release.js
@@ -5,6 +5,6 @@ console.log("Uploading to R2...");
 await $`pnpm wrangler r2 object put static/sponsors.svg --file=sponsorkit/sponsors.svg`;
 
 console.log("Updating repo...");
-await $`git add . && git commit -m "Update sponsors.svg" && git push`;
+await $`git add . && git commit -m "Update sponsors.png" && git push`;
 
 console.log("Done!");
